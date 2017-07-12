16241225968772c461d127e57b4d32a05a5f45cc
lairon10 <lairon10@outlook.com>
2017-07-12 15:45:49 -0300

    Initial check-in of module MKClickTool

---
+++ MKTeste/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+
+namespace MKClickTool
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+
+			Console.WriteLine("===ATUALIZAÇÃO EM MASSA MIKROTIK - Developer By Lairon Costa===");
+			Console.WriteLine("Digite a classe de ip (/24): ");
+			string ip = Console.ReadLine();
+			Console.WriteLine("Digite o usuário: ");
+			string usuario = Console.ReadLine();
+			Console.WriteLine("Digite a senha: ");
+			string senha = Console.ReadLine();
+
+			MK mikrotik = new MK(ip);
+
+			if (mikrotik.Login(usuario, senha))
+			{
+				mikrotik.Send("/ip/address/add");
+				mikrotik.Send("=address=192.168.0.1/24");
+				mikrotik.Send("=interface=ether2");
+				mikrotik.Send(".tag=ip", true);
+
+				foreach (string h in mikrotik.Read())
+				{
+					Console.WriteLine(h);
+					Console.ReadKey();
+				}
+			}
+			else {
+				Console.WriteLine("Nao Foi Possivel Fazer Login! - Usuario ou Senha Incorreta");
+				mikrotik.Close();
+			}
+
+		}
+	}
+}
